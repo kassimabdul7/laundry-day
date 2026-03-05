e5bccbaec1375d0273f4919f95d3b83a65307f67
kassimabdul7 <kassimabdul7@gmail.com>
2026-03-04 21:17:09 -0800

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Laundry Day Laundromats",
+  description: "Modern Bay Area Laundromats"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, fontFamily: "Arial" }}>
+        {children}
+      </body>
+    </html>
+  );
+}
